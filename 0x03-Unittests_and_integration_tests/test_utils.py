2081f20d4d6f75b1d6e25aa79d7d51b4447b832e
Bashu-011 <bashu2609@gmail.com>
2025-09-17 15:30:18 +0300

    Task 1, unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.py (Task 0: parameterize access_nested_map)."""
+"""Unit tests for utils.py(Task 0)"""
 
 import unittest
 from typing import Mapping, Sequence, Any, Dict, Tuple
@@ -8,7 +8,7 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests for access_nested_map function."""
+    """Tests for access_nested_map function"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -21,9 +21,32 @@ class TestAccessNestedMap(unittest.TestCase):
         path: Sequence[str],
         expected: Any
     ) -> None:
-        """It should return the value found by following the key path."""
+        """It should return the value found by following path"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-        # Body is 1 line of assertion as required.
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests for access_nested_map function"""
+
+    @parameterized.expand([
+        ( {}, ("a",), "a" ),
+        ( {"a": 1}, ("a", "b"), "b" ),
+    ])
+    def test_access_nested_map_exception(
+        self,
+        nested_map: Mapping[str, Any],
+        path: Sequence[str],
+        missing_key: str
+    ) -> None:
+        """Test that a KeyError is raised for missing key"""
+        
+        #assertRaises to check that a KeyError is raised
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+
+        # Check keyError message is as expected
+        self.assertEqual(str(cm.exception), f"'{missing_key}'")
+
 
 
 if __name__ == "__main__":
