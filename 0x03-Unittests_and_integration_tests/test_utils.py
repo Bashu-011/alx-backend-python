14b6663e406e8c6c1d92ef069531d942e9609e48
Bashu-011 <bashu2609@gmail.com>
2025-09-17 16:22:27 +0300

    Task 4, unit tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from unittest.mock import patch, MagicMock
 from utils import get_json
 from utils import access_nested_map
 from utils import memoize
+from client import GithubOrgClient
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -109,6 +110,37 @@ class TestMemoize(unittest.TestCase):
         self.assertEqual(result_1, result_2)
 
 
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests for the GithubOrgClient class"""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")  #get_json function
+    def test_org(self, org_name, mock_get_json):
+        """Test that the org method returns the correct value and calls get_json once"""
+
+        #define the expected payload
+        test_payload = {"repos_url": f"https://api.github.com/orgs/{org_name}/repos"}
+
+        #configure the mock to return the payload
+        mock_get_json.return_value = test_payload
+
+        #instansiate githubOrgclient
+        client = GithubOrgClient(org_name)
+
+        #call the org method
+        result = client.org()
+
+        #check get_json() is called once
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+        #assert the result
+        self.assertEqual(result, test_payload)
+
+
+
 if __name__ == "__main__":
     unittest.main()
-    
+
