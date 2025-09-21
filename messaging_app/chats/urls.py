64bca9036b46b51cd8fefa7bcbeca36289876987
Bashu-011 <bashu2609@gmail.com>
2025-09-21 13:14:57 +0300

    updated urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,16 +3,22 @@
 from django.urls import path, include
 from messaging_app.chats import admin
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter  
 from .views import ConversationViewSet, MessageViewSet
 
-#creating router and registering views
+# Create the Default Router
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
 
-#api routes are assigned automatically
+# Create the Nested Router for messages under a conversation
+conversation_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
-    path('', include(router.urls)),  
+    path('', include(router.urls)), 
+    path('', include(conversation_router.urls)), 
     path('admin/', admin.site.urls),
-    path("api/", include('messaging_app.chats.urls')), 
+    path('api/', include('messaging_app.chats.urls')),  
+    path('api-auth/', include('rest_framework.urls')),  
 ]
+
