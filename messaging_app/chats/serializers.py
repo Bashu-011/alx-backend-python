9c3cac8155ccc049a80e733a531f3be8c52f6ed6
Bashu-011 <bashu2609@gmail.com>
2025-09-21 12:30:05 +0300

    Updated the relationships more explicitly

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,3 +1,5 @@
+# messaging_app/chats/serializers.py
+
 from rest_framework import serializers
 from .models import User, Message, Conversation
 
@@ -6,18 +8,24 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'created_at']
 
-
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer()  #for the foreign key
+    sender = UserSerializer()  #for the FK relationship
+
+    #SerializerMethodField() to add custom field
+    sender_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender', 'sender_name', 'message_body', 'sent_at']
+
 
 class ConversationSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(many=True)  #for the foreign keys
-    messages = MessageSerializer(many=True) 
+    participants = UserSerializer(many=True)  
+    messages = MessageSerializer(many=True)  #for the FK relationships
+
+    total_messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants', 'messages', 'created_at']
+        fields = ['conversation_id', 'participants', 'messages', 'created_at', 'total_messages']
+
