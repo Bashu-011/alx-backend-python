f8a289b51a1796e38678cc33ce8e145198ee9197
Bashu-011 <bashu2609@gmail.com>
2025-09-21 12:12:06 +0300

    Created the models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+import uuid
+from django.db import models 
 
-# Create your models here.
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    role_choices = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=10, choices=role_choices, default='guest')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+    
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} at {self.sent_at}"
+    
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.conversation_id} created at {self.created_at}"
