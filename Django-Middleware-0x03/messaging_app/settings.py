bdf37647c6f8328583f8489df2a09674d200c9c9
Bashu-011 <bashu2609@gmail.com>
2025-09-28 20:04:27 +0300

    Middleware - updated the settings.py

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -74,6 +74,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'messaging_app.chats.middleware.RequestLoggingMiddleware', 
+    'chats.middleware.RequestLoggingMiddleware',
 ]
 
 AUTH_USER_MODEL = 'chats.User'
