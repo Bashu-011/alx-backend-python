9b1fba85ab6086f0a1e18b896af366e43aaa403f
Bashu-011 <bashu2609@gmail.com>
2025-09-29 10:16:27 +0300

    Signals | Task 1 | Updated MessageHistory to include edited_by

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -26,6 +26,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="history")
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_histories')
 
     def __str__(self):
         return f"History for Message {self.message.id} at {self.edited_at}"
