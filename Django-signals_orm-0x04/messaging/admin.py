2cfca3954a4024aab82b7850ef088dafab78bf39
Bashu-011 <bashu2609@gmail.com>
2025-09-29 10:11:20 +0300

    Signals | Task 1

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Message, Notification
+from .models import Message, MessageHistory, Notification
 
 admin.site.register(Message)
 admin.site.register(Notification)
+#add message history signal
+admin.site.register(MessageHistory)
+
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,15 +1,35 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, MessageHistory, Notification
 
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     """
-    a notification for the receiver when a new message is created
+    Create a notification for the receiver when a new message is created.
     """
-    if created:  
+    if created:
         Notification.objects.create(
             user=instance.receiver,
             message=instance
         )
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """
+    Before a Message is updated, save the old content to MessageHistory.
+    """
+    if instance.pk:  #check if message exists, not on new messages
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                #log previous content
+                MessageHistory.objects.create(
+                    message=old_message,
+                    old_content=old_message.content
+                )
+                #mark the message as edited
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
